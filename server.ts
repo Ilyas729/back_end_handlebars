6a8ce6086f8e4aeb7900ae03a4b0c17ed72a68d9
mirzobeck <116816281+uzbsobirov@users.noreply.github.com>
2023-03-25 12:34:13 +0500

    2-posts

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
-import express from 'express'
+import express, { request, response } from 'express'
 import cors from 'cors'
 import { engine } from 'express-handlebars'
+import  sqlite3  from 'sqlite3'
 
 const server = express()
+
+const db = new sqlite3.Database('./posts.db')
 
 server.use(cors())
 server.use(express.json())
@@ -13,7 +16,36 @@ server.set('view engine', '.hbs')
 server.set('views', './pages')
 
 server.get('/', (request, response) => {
-    response.render('index')
+    db.all('SELECT * FROM post;', (error, rows) => {
+        console.log(error);
+        response.render('index', {
+            posts:rows
+        })
+    })
 })
 
+server.get('/add-post', (request, response) => {
+    response.render('add-post')
+})
+
+server.post('/new-post', (request, response) => {
+    const date =new Date()
+    const sql = 'INSERT INTO post (nickname, content, date) VALUES (?,?,?);'
+    const params = [
+        request.body.nickname,
+        request.body.content,
+        new Date().toLocaleString()
+    ]
+    db.run(sql, params, (error) => {
+        console.log(error);
+        response.redirect('/')
+     })
+})
+
+server.get('/delete/:id', (request, response) => {
+    db.run('DELETE FROM post WHERE id = ?', [request.params.id], (error) => {
+        console.log(error);
+        response.redirect('/')
+    })
+})
 server.listen(8080, () => console.log("Server is running on port 8080"))
